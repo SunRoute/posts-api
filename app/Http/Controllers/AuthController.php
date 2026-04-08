f33a623b47e09d8e5113fc976145ad34608a41bf
SunRoute <sunroute@proton.me>
2026-04-08 19:24:59 +0200

    Auth controller-route-migration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json($user);
+    }
+}
